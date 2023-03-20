e48725d76d2631b7f773d8922bdcd01d16abab78
Eric BREHAULT <ebrehault@gmail.com>
2023-03-20 09:21:09 +0100

    fix api

--- src/abfab/plone/browser/api.py
+++ src/abfab/plone/browser/api.py
@@ -6,6 +6,7 @@ from souper.interfaces import ICatalogFactory
 from repoze.catalog.query import Eq
 from plone import api
 from datetime import datetime
+from pathlib import Path
 from .catalog import CatalogFactory
 import json
 import mimetypes
@@ -37,6 +38,15 @@ class AbFabTraverser(object):
     def GET(self):
         path = self.get_path()
         accept = self.request.get_header('Accept')
+        if path.endswith('/@basic'):
+            return self.view_basic(self.get_path(raw=False))
+        if path.endswith('/@edit-data'):
+            obj = self.get_object(self.get_path(raw=False))
+            return self.view_source(obj, 'text/plain')
+        if path.endswith('/@edit'):
+            path = self.get_path(raw=False)
+            editor_component = self.get_object('/abfab/editor/editor.svelte')
+            return self.wrap_component(editor_component, path + '/@edit-data', 'text')
         if path.endswith('.svelte') and 'raw' not in self.request:
             path += '.js'
             js_component = self.get_object(path)
@@ -69,11 +79,31 @@ class AbFabTraverser(object):
             self.request.response.setStatus(400)
             return {"error": "id is missing"}
         path = "/".join([''] + self.path + [id])
-        record = self.get_object(path) or Record()
-        record.attrs["path"] = path
+        existing = False
+        record = self.get_object(path)
+        if record:
+            existing = True
+        else:
+            record = Record()
+            record.attrs["path"] = path
         for key, value in data.items():
             record.attrs[key] = value
-        self.soup.add(record)
+        if not existing:
+            self.soup.add(record)
+        self.set_last_modified()
+        self.request.response.setHeader('Content-Type', 'application/json')
+        return {"path": path}
+    
+    def PATCH(self):
+        body = self.request.get('BODY')
+        data = json.loads(body)
+        path = self.get_path()
+        record = self.get_object(path)
+        if not record:
+            self.request.response.setStatus(404)
+            return "Record not found"
+        for key, value in data.items():
+            record.attrs[key] = value
         self.set_last_modified()
         self.request.response.setHeader('Content-Type', 'application/json')
         return {"path": path}
@@ -83,9 +113,21 @@ class AbFabTraverser(object):
         for resource in self.soup.query(Eq('path', path)):
             del self.soup[resource]
 
-    def get_path(self):
+    def HEAD(self):
+        path = self.get_path()
+        object = self.get_object(path)
+        if object:
+            self.request.response.setStatus(200)
+            return ""
+        else:
+            self.request.response.setStatus(404)
+            return "Record not found"
+
+    def get_path(self, raw=True):
         path = self.path
         if path[-1] == self.request.method:
+            path = path[:-1]
+        if not raw and path[-1].startswith('@'):
             path = path[:-1]
         return "/".join([''] + path)
     
@@ -147,6 +189,14 @@ class AbFabTraverser(object):
             content_type = mimetypes.guess_type(object.attrs['path'])[0]
         self.request.response.setHeader('Content-Type', content_type)
         return object.attrs['file']
+ 
+    def view_basic(self, path):
+        # import pdb; pdb.set_trace()
+        object = self.get_object(path)
+        if not object:
+            return {"type": "Directory", "path": path}
+        else:
+            return {"type": "File", "path": path}
 
     def view_json(self, object):
         self.request.response.setHeader('Content-Type', 'application/json')
@@ -171,3 +221,36 @@ class Reset(object):
         soup = get_soup('abfab', self.context)
         provideUtility(CatalogFactory(), ICatalogFactory, name='abfab')
         soup.clear()
+
+
+class Tree(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        soup = get_soup('abfab', self.context)
+        provideUtility(CatalogFactory(), ICatalogFactory, name='abfab')
+        search = [r for r in soup.query(Eq('path', '/'))]
+        results = self.get_path_dict([r.attrs['path'] for r in search])
+        self.request.response.setHeader('Content-Type', 'application/json')
+        return json.dumps(results)
+    
+    def get_path_dict(self, paths):
+        """Builds a tree like structure out of a list of paths"""
+        def _recurse(dic, fullpath, chain):
+            if len(chain) == 0:
+                return
+            if len(chain) == 1:
+                dic[chain[0]] = {"type": "File", "path": fullpath}
+                return
+            key, *new_chain = chain
+            if key not in dic:
+                dic[key] = {}
+            _recurse(dic[key], fullpath, new_chain)
+            return
+
+        new_path_dict = {}
+        for path in paths:
+            _recurse(new_path_dict, path, Path(path).parts)
+        return new_path_dict
