058416369a9fa914e6eadff3906abac025fe8639
Eric BREHAULT <ebrehault@gmail.com>
2023-10-03 14:30:24 +0200

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='abfab.plone',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description="AbFab for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
